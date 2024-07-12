0269d5234bd472e6ce72621a853ffa014482691d
Vinícius Pereira Polli <viniciusppolli07@gmail.com>
2024-07-12 01:47:09 -0300

    Primeira Versão

---
+++ README.md
@@ -0,0 +1,35 @@
+
+# Projeto de App de Previsão do Tempo
+
+Este projeto que tem por objetivo retornar a previsão do tempo.
+
+Este projeto foi desenvolvido por eu Vinícius.
+
+
+## Tecnologias Usadas
+
+![HTML5](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
+
+![CSS3](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
+
+![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)
+
+![Git](https://img.shields.io/badge/GIT-E44C30?style=for-the-badge&logo=git&logoColor=white)
+
+
+## API
+
+*A API usada foi a OpenWeatherMap*
+
+![LINK DA API](https://openweathermap.org/api)
+
+## **Minhas Redes Sociais**
+
+- [Linkedin](https://www.linkedin.com/in/vinicius-pereira-polli17)
+- [Instagram](https://www.instagram.com/eu_viniipp/)
+- [Site Pessoal](https://viniipp.github.io/New-Portifolio/)
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,68 @@
+// index.js
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+
+search.addEventListener('click', () => {
+    const APIKey = '0e7d996a0085f497a3607493d4e1be38';
+    const cidade = document.querySelector('.search-box input').value;
+
+    if (cidade === '') {
+        return;
+    }
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${APIKey}&lang=pt_br`)
+        .then(response => response.json())
+        .then(json => {
+            if (json.cod === '404') {
+                container.style.height = '400px';
+                weatherBox.style.display = 'none';
+                weatherDetails.style.display = 'none';
+                error404.style.display = 'block';
+                error404.classList.add('fadeIn');
+                return;
+            }
+
+            error404.style.display = 'none';
+            error404.classList.remove('fadeIn');
+
+            const image = document.querySelector('.weather-box img');
+            const temperatura = document.querySelector('.weather-box .temperatura');
+            const descricao = document.querySelector('.weather-box .descricao');
+            const umidade = document.querySelector('.weather-details .umidade span');
+            const vento = document.querySelector('.weather-details .vento span');
+
+            switch (json.weather[0].main) {
+                case 'Clear':
+                    image.src = 'images/clear.png';
+                    break;
+                case 'Rain':
+                    image.src = 'images/rain.png';
+                    break;
+                case 'Snow':
+                    image.src = 'images/snow.png';
+                    break;
+                case 'Clouds':
+                    image.src = 'images/cloud.png';
+                    break;
+                case 'Mist':
+                    image.src = 'images/mist.png';
+                    break;
+                default:
+                    image.src = ''; // Caminho padrão para imagens
+            }
+
+            temperatura.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+            descricao.innerHTML = `${json.weather[0].description}`;
+            umidade.innerHTML = `${json.main.humidity}%`;
+            vento.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
+            weatherBox.style.display = '';
+            weatherDetails.style.display = '';
+            weatherBox.classList.add('fadeIn');
+            weatherDetails.classList.add('fadeIn');
+            container.style.height = '590px';
+        });
+});
